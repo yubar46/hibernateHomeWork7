54c2a3bebb91f13dc31a46d75dceb3422e6adfc6
yubar46 <yubar4.6@gmail.com;git config --global user.email yubar4.6@gmail.com>
2022-06-06 05:46:34 +0430

    create class ProductType

---
+++ src/main/java/domain/ProductType.java
@@ -0,0 +1,34 @@
+package domain;
+
+import java.util.List;
+
+public class ProductType {
+
+    private String name;
+    private int id;
+    private List<Attribute> attributes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Attribute> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(List<Attribute> attributes) {
+        this.attributes = attributes;
+    }
+}
