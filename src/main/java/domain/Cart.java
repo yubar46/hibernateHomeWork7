05efc2bc0622e56c3c5bbbe5361f5792a3c98928
yubar46 <yubar4.6@gmail.com;git config --global user.email yubar4.6@gmail.com>
2022-06-06 05:47:56 +0430

    create class Cart

---
+++ src/main/java/domain/Cart.java
@@ -0,0 +1,61 @@
+package domain;
+import java.util.ArrayList;
+
+public class Cart {
+    private int cartPrice;
+    private int UserId;
+    int id;
+
+    Address address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private ArrayList<CartItem> products = new ArrayList<CartItem>(5);
+
+    public Cart() {
+
+    }
+
+    public void calculateCartPrice() {
+        cartPrice = 0;
+        for (int i = 0; i < products.size(); i++) {
+
+
+            cartPrice += products.get(i).getAllPrice();
+
+        }
+
+    }
+
+    public int getCartPrice() {
+        return cartPrice;
+    }
+
+    public void setCartPrice(int cartPrice) {
+        this.cartPrice = cartPrice;
+    }
+
+
+    public int getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(int userId) {
+        UserId = userId;
+    }
+
+
+    public ArrayList<CartItem> getProducts() {
+        return products;
+    }
+
+    public void setProducts(CartItem cartItem) {
+        this.products.add(cartItem);
+    }
+}
